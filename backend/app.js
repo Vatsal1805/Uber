70bf8aa92ad4c24dcc38fc796159444704d2cc5a
Vatsal1805 <vatsalbhavsar2011@gmail.com>
2025-03-30 17:14:37 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const dotenv = require('dotenv')
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+
+
+
+
+
+
+
+
+
+
+module.esports = app;
